b8170487b18c3bc84a3104c2f5504f0c17cb0ba5
ALEXIS CARBILLET <alexiscarbillet@users.noreply.github.com>
2024-11-27 15:56:05 -0500

    add 10th app

--- js/structure.js
+++ js/structure.js
@@ -64,3 +64,98 @@ function toggleVisibility(listId) {
   }
 }
 
+function updateAppCount() {
+  const listItems = document.querySelectorAll('#android-list li');
+  const count = listItems.length;
+  document.getElementById('app-count').textContent = count;
+
+  const listItems2 = document.querySelectorAll('#firefox-list li');
+  const count2 = listItems2.length;
+  document.getElementById('firefox-count').textContent = count2;
+
+  const listItems3 = document.querySelectorAll('#website-list li');
+  const count3 = listItems3.length;
+  document.getElementById('website-count').textContent = count3;
+
+  // Python part
+  const listItems4 = document.querySelectorAll('#python-list1 li');
+  const count4 = listItems4.length;
+  document.getElementById('python1-count').textContent = count4;
+
+  const listItems5 = document.querySelectorAll('#python-list2 li');
+  const count5 = listItems5.length;
+  document.getElementById('python2-count').textContent = count5;
+
+  const listItems6 = document.querySelectorAll('#python-list3 li');
+  const count6 = listItems6.length;
+  document.getElementById('python3-count').textContent = count6;
+
+  const listItems7 = document.querySelectorAll('#python-list4 li');
+  const count7 = listItems7.length;
+  document.getElementById('python4-count').textContent = count7;
+
+  const listItems8 = document.querySelectorAll('#python-list5 li');
+  const count8 = listItems8.length;
+  document.getElementById('python5-count').textContent = count8;
+
+  const listItems9 = document.querySelectorAll('#python-list6 li');
+  const count9 = listItems9.length;
+  document.getElementById('python6-count').textContent = count9;
+
+  const listItems10 = document.querySelectorAll('#python-list7 li');
+  const count10 = listItems10.length;
+  document.getElementById('python7-count').textContent = count10;
+
+  const listItems11 = document.querySelectorAll('#python-list8 li');
+  const count11 = listItems11.length;
+  document.getElementById('python8-count').textContent = count11;
+
+  // html
+  const listItems12 = document.querySelectorAll('#html-list li');
+  const count12 = listItems12.length;
+  document.getElementById('html-count').textContent = count12;
+
+  //bash
+  const listItems13 = document.querySelectorAll('#bash-list li');
+  const count13 = listItems13.length;
+  document.getElementById('bash-count').textContent = count13;
+
+  //go
+  const listItems14 = document.querySelectorAll('#go-list li');
+  const count14 = listItems14.length;
+  document.getElementById('go-count').textContent = count14;
+
+  //android
+  const listItems15 = document.querySelectorAll('#android2-list li');
+  const count15 = listItems15.length;
+  document.getElementById('android-count2').textContent = count15;
+}
+
+// Call the function on page load to set the initial count
+updateAppCount();
+
+// Function to update the total article count
+function updateTotalArticleCount() {
+  const python1Count = document.getElementById('python1-count');
+  const python2Count = document.getElementById('python2-count');
+  const python3Count = document.getElementById('python3-count');
+  const python4Count = document.getElementById('python4-count');
+  const python5Count = document.getElementById('python5-count');
+  const python6Count = document.getElementById('python6-count');
+  const python7Count = document.getElementById('python7-count');
+  const python8Count = document.getElementById('python8-count');
+
+  // Get the total count and update the total articles span
+  const totalArticles = parseInt(python1Count.textContent) + parseInt(python2Count.textContent) + parseInt(python3Count.textContent) + parseInt(python4Count.textContent) + parseInt(python5Count.textContent) + parseInt(python6Count.textContent) + parseInt(python7Count.textContent) + + parseInt(python8Count.textContent);
+  document.getElementById('python-total-count').textContent = totalArticles;
+
+  const htmlCount = document.getElementById('html-count');
+  const bashCount = document.getElementById('bash-count');
+  const goCount = document.getElementById('go-count');
+
+  const totalArticles2 = totalArticles + parseInt(htmlCount.textContent) + parseInt(bashCount.textContent) + parseInt(goCount.textContent);
+  document.getElementById('coding-total-count').textContent = totalArticles2;
+}
+
+// Call the function on page load to set the initial counts
+updateTotalArticleCount();
